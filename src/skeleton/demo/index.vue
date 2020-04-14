8f5453de3b415f2fba87030243c8142e595d4f67
xakoy <xakoy@qq.com>
2020-04-14 16:27:08 +0800

    修改skeleton

--- src/skeleton/demo/index.vue
+++ src/skeleton/demo/index.vue
@@ -1,16 +1,16 @@
 <template>
   <demo-section>
     <demo-block :title="t('basicUsage')">
-      <van-skeleton title :row="3" />
+      <bvan-skeleton title :row="3" />
     </demo-block>
 
     <demo-block :title="t('showAvatar')">
-      <van-skeleton title avatar :row="3" />
+      <bvan-skeleton title avatar :row="3" />
     </demo-block>
 
     <demo-block :title="t('showChildren')">
-      <van-switch v-model="show" size="24px" />
-      <van-skeleton title avatar :row="3" :loading="!show">
+      <bvan-switch v-model="show" size="24px" />
+      <bvan-skeleton title avatar :row="3" :loading="!show">
         <div class="demo-preview">
           <img src="https://img.yzcdn.cn/vant/logo.png" />
           <div class="demo-content">
@@ -18,7 +18,7 @@
             <p>{{ t('desc') }}</p>
           </div>
         </div>
-      </van-skeleton>
+      </bvan-skeleton>
     </demo-block>
   </demo-section>
 </template>
@@ -55,7 +55,7 @@ export default {
 .demo-skeleton {
   background-color: @white;
 
-  .van-switch {
+  .@{module}-switch {
     margin: 0 @padding-md @padding-xs;
   }
 
